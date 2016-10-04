13376be7f28a95d0f4139bdd11b9e7f25d6d2567
Lance Gonzalez <Lance.gonzalez7@Gmail.com>
2016-10-04 00:15:32 -0600

    finish mini project

---
+++ js/products/productsCtrl.js
@@ -0,0 +1,17 @@
+angular.module('miniRouting').controller('productsCtrl',function($scope, $stateParams, productService) {
+
+  if ($stateParams.id === 'shoes') {
+    $scope.productData = productService.shoeData;
+  }
+  else if ($stateParams.id === 'socks') {
+    $scope.productData = productService.sockData;
+  }
+});
+angular.module('miniRouting').controller('productsCtrl', function ($scope, $stateParams, productService) {
+    if ($stateParams.id === 'shoes') {
+        $scope.productData = productService.shoeData;
+    }
+    else if ($stateParams.id === 'socks') {
+        $scope.productData = productService.sockData;
+    }
+});
